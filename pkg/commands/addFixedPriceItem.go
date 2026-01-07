05aaebc287f7d952fedaf8a9d4e100394d782409
dracoDevs <Mickiel.araya@gmail.com>
2026-01-07 18:12:52 -0500

    .

--- pkg/commands/addFixedPriceItem.go
+++ pkg/commands/addFixedPriceItem.go
@@ -15,7 +15,6 @@ type AddFixedPriceItem struct {
 	Description           string `xml:",omitempty"`
 	StartPrice            string
 	ListingType           string            `xml:",omitempty"`
-	BrandMPN              BrandMPN          `xml:",omitempty"`
 	Quantity              uint              `xml:",omitempty"`
 	BestOfferDetails      *BestOfferDetails `xml:",omitempty"`
 	PaymentMethods        string            `xml:",omitempty"`
